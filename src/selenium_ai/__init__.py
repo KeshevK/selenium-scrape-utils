47f5b1d366c0c0fad7b808d9cb7ea0ab92235370
Keshev <keshevkulkarni@gmail.com>
2020-12-09 01:06:59 -0500

    removed old files

--- src/selenium_ai/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Top-level package for seleniumAI."""
-
-__author__ = """Keshev Kulkarni"""
-__email__ = 'localized.analytics@gmail.com'
-__version__ = '0.1.2'
-
-from .ElementFinder import ElementFinder
---
+++ tests/test_selenium_scrape_utils.py
@@ -0,0 +1,5 @@
+from selenium_scrape_utils import selenium_scrape_utils
+
+
+def test_selenium_scrape_utils():
+    assert selenium_scrape_utils is not None
